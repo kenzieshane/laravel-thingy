6d200b131bf4d7aabbc02e2882e6a4350c5725e9
kenzieshane <111677159+kenzieshane@users.noreply.github.com>
2026-02-20 09:06:55 +0700

    Yes sire

--- app/Http/Controllers/Pengguna.php
+++ app/Http/Controllers/Pengguna.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class Pengguna extends Controller
 {
-    //
+    public function register(): string {
+        $request = request();
+        $incomingFields = $request->validate(rules: [
+            'name' => 'required',
+            'password' => 'required',
+            'email' => 'required',
+        ]);
+        return 'hello from our controller!';
+    }
 }
